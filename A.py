4f70fb4b52514bf05f07e71b61618e216b0e8ee6
Bianca Csan <ebcsan42@hotmail.com>
2020-04-12 15:10:31 +0000

    commit A.py

---
+++ A.py
@@ -0,0 +1,16 @@
+a = int(input())
+for i in range(a):
+    teste = input()
+    if 'A' in teste:
+        if 'J' in teste:
+            if 'K' in teste:
+                if 'Q' in teste:
+                    print("Aaah muleke")
+                else: 
+                    print("Ooo raca viu")
+            else:
+                print("Ooo raca viu")
+        else:
+            print("Ooo raca viu")                    
+    else:
+        print("Ooo raca viu")
